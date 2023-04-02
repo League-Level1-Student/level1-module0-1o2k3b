6ca3dec3faccdb6a372794d2952dc695a58cd49a
root <root@7b28bf3a9483>
2023-04-02 19:55:46 +0000

    0t7

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,35 +26,44 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+String holycow = "https://i.pinimg.com/736x/fb/0e/b5/fb0eb5ca2491dd57e737705c615b723a.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component Imagee = createImage(holycow);
 		// 3. use the "createImage()" method below to initialize your Component
-
+quizWindow.add(Imagee);
 		// 4. add the image to the quiz window
 
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String answer = JOptionPane.showInputDialog("what animal is this");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(answer.equalsIgnoreCase("holy cow")) {
+	JOptionPane.showMessageDialog(null, "Holy cow, you got it");
+} else {
+	JOptionPane.showMessageDialog(null, "you messd up, now da greater cow (holy cow) shall kill you");
+}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(Imagee);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+String fits = "https://i.pinimg.com/236x/1d/38/71/1d38713804ad0c4dd97d50623a01ed83--food-bowl-food-networktrisha.jpg";
+Component image = createImage(fits);
 		// 11. add the second image to the quiz window
-
+quizWindow.add(image);
 		// 12. pack the quiz window
-
+quizWindow.pack();
 		// 13. ask another question
-
+String answe2 = JOptionPane.showInputDialog("what animal is curled up");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+if(answe2.equals("cat")) {
+	JOptionPane.showMessageDialog(null, "yu gotted it");
+} else {
+	JOptionPane.showMessageDialog(null, "yu scred up");
+}
 	}
 
 	private Component createImage(String imageUrl) {
